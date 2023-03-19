f0bafaa56a4e8c2939551170dba7c200b7c860be
Ilja Lifshic <kriptorab608@gmail.com>
2023-03-19 20:50:54 +0200

    new

---
+++ e-shop-front/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.logo {
+  display: block;
+}
+</style>
